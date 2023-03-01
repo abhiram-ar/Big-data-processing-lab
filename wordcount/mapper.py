9991beda831de67d93cbddc86387375f3d36ade2
abhiram-ar <abhiram2001.ars@gmail.com>
2023-03-01 11:55:58 +0530

    -m

--- wordcount/mapper.py
+++ wordcount/mapper.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
+
+# import sys because we need to read and write data to STDIN and STDOUT
 import sys
 
+# reading entire line from STDIN (standard input)
 for line in sys.stdin:
-    line = line.strip()
-    words = line.split()
-
-    for word in words:
-        print ('%s\t%s' % (word, 1))
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
+	# to remove leading and trailing whitespace
+	line = line.strip()
+	# split the line into words
+	words = line.split()
+	
+	# we are looping over the words array and printing the word
+	# with the count of 1 to the STDOUT
+	for word in words:
+		# write the results to STDOUT (standard output);
+		# what we output here will be the input for the
+		# Reduce step, i.e. the input for reducer.py
+		print ('%s\t%s' % (word, 1))
